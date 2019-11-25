be9400e4f4e212c26336dd1e6d0995d1b8705bf6
super-model-4696 <jtb6179@g.rit.edu>
2019-11-25 04:47:56 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -7,5 +7,13 @@ def meal_choice(veg1, veg2, protein = 'meat')
    return meal_choice
  end
 
+<<<<<<< HEAD
 
+=======
+def meal_choice(veg1,veg2, protein = 'tofu')
+  meal_choice = "A plate of #{protein} with #{veg1} and #{veg2}."
+   puts meal_choice
+   return meal_choice
+end
+>>>>>>> 9397b1b7c1c5df8af791b9b0f631cdb740d3a9f7
 
